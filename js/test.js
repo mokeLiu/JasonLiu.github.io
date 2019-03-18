29ec160715c05f78ed3bf531456a4d170352a7c8
mokeLiu <mokeLiu@users.noreply.github.com>
2019-03-18 09:40:49 +0800

    改动了一点

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
-Bomb.initialize("0ed89ba049534af505fc0324c3a70cc4", "185ee4b4ab9e2a77f9661f61a17e3e56");
+window.onload = function(){
+	Bomb.initialize("0ed89ba049534af505fc0324c3a70cc4", "185ee4b4ab9e2a77f9661f61a17e3e56");
 
-var GameScore = Bmob.Object.extend("GameScore");
-var gameScore = new GameScore();
-gameScore.set("score", 1337);
-gameScore.save(null, {
-	success: function(object) {
-		console.log("create object success, object id:" + object.id);
-	},
-	error: function(model, error) {
-		console.log("create object fail");
-	}
-});
+	const query = Bmob.Query('testTable');
+	query.set("name","Bmob");
+	query.set("count",1);
+	query.set("score",99.9);
+	query.save().then(res => {
+	  console.log(res)
+	}).catch(err => {
+	  console.log(err)
+	})
+}
