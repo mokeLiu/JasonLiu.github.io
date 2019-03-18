5d9cf6ea8ed597a3030bde21749c4e8934b75180
mokeLiu <mokeLiu@users.noreply.github.com>
2019-03-18 09:17:26 +0800

    替换为了Bomb

--- js/test.js
+++ js/test.js
@@ -1,7 +1,13 @@
-(function(){
-	const pastScore = document.getElementById('past_score');
+Bomb.initialize("0ed89ba049534af505fc0324c3a70cc4", "185ee4b4ab9e2a77f9661f61a17e3e56");
 
-	const dbRefObject = firebase.database().ref().child('object');
-
-	dbRefObject.on('value', snap => console.log(snap.val()));
-}());
+var GameScore = Bmob.Object.extend("GameScore");
+var gameScore = new GameScore();
+gameScore.set("score", 1337);
+gameScore.save(null, {
+	success: function(object) {
+		console.log("create object success, object id:" + object.id);
+	},
+	error: function(model, error) {
+		console.log("create object fail");
+	}
+});
